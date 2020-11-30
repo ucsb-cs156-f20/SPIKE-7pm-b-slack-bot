890fa3b3cd4267b60d9196def2dbdce3dfc2c6ac
Phillip Conrad <pconrad@cs.ucsb.edu>
2020-11-30 15:37:53 -0800

    pc - refactor slash command controller

--- src/main/java/edu/ucsb/changeme/controllers/SlackSlashCommandController.java
+++ src/main/java/edu/ucsb/changeme/controllers/SlackSlashCommandController.java
@@ -1,5 +1,8 @@
 package edu.ucsb.changeme.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.ramswaroop.jbot.core.slack.models.Attachment;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.ucsb.changeme.models.SlackSlashCommandParams;
+
 /**
  * Sample Slash Command Handler.
  *
@@ -26,17 +31,16 @@ public class SlackSlashCommandController {
     private static final Logger logger = LoggerFactory.getLogger(SlackSlashCommandController.class);
 
     /**
-     * The token you get while creating a new Slash Command. You
-     * should paste the token in application.properties file.
+     * The token you get while creating a new Slash Command. You should paste the
+     * token in application.properties file.
      */
     @Value("${slashCommandToken}")
     private String slackToken;
 
-
     /**
-     * Slash Command handler. When a user types for example "/app help"
-     * then slack sends a POST request to this endpoint. So, this endpoint
-     * should match the url you set while creating the Slack Slash Command.
+     * Slash Command handler. When a user types for example "/app help" then slack
+     * sends a POST request to this endpoint. So, this endpoint should match the url
+     * you set while creating the Slack Slash Command.
      *
      * @param token
      * @param teamId
@@ -50,48 +54,121 @@ public class SlackSlashCommandController {
      * @param responseUrl
      * @return
      */
-    @RequestMapping(value = "/api/public/slash-command",
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @RequestMapping(value = "/api/public/slash-command", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public RichMessage onReceiveSlashCommand(@RequestParam("token") String token,
-                                             @RequestParam("team_id") String teamId,
-                                             @RequestParam("team_domain") String teamDomain,
-                                             @RequestParam("channel_id") String channelId,
-                                             @RequestParam("channel_name") String channelName,
-                                             @RequestParam("user_id") String userId,
-                                             @RequestParam("user_name") String userName,
-                                             @RequestParam("command") String command,
-                                             @RequestParam("text") String text,
-                                             @RequestParam("response_url") String responseUrl) {
+            @RequestParam("team_id") String teamId, @RequestParam("team_domain") String teamDomain,
+            @RequestParam("channel_id") String channelId, @RequestParam("channel_name") String channelName,
+            @RequestParam("user_id") String userId, @RequestParam("user_name") String userName,
+            @RequestParam("command") String command, @RequestParam("text") String text,
+            @RequestParam("response_url") String responseUrl) {
         // validate token
-
         logger.info("slash command processing...");
         if (!token.equals(slackToken)) {
             return new RichMessage("Sorry: the slack bot received an invalid token.");
         }
 
-        /** build response */
-        RichMessage richMessage = new RichMessage("The is Slash Commander!");
-        richMessage.setResponseType("in_channel");
-        // set attachments
-        int numAttachments = 8;
-        Attachment[] attachments = new Attachment[numAttachments];
-        for (int i=0; i<numAttachments; i++)
-            attachments[i] = new Attachment();
-        attachments[0].setText(String.format("team_id=%s",teamId));
-        attachments[1].setText(String.format("team_domain=%s",teamDomain));
-        attachments[2].setText(String.format("channel_id=%s",channelId));
-        attachments[3].setText(String.format("channel_name=%s",channelName));
-        attachments[4].setText(String.format("user_id=%s",userId));
-        attachments[5].setText(String.format("user_name=%s",userName));
-        attachments[6].setText(String.format("command=%s",command));
-        attachments[7].setText(String.format("text=%s",text));
+        SlackSlashCommandParams params = new SlackSlashCommandParams();
 
-        richMessage.setAttachments(attachments);
-        
+        params.setToken(token);
+        params.setTeamId(teamId);
+        params.setTeamDomain(teamDomain);
+        params.setChannelId(channelId);
+        params.setChannelName(channelName);
+        params.setUserId(userId);
+        params.setCommand(command);
+        params.setText(text);
+        params.setResponseUrl(responseUrl);
+
+        String textParts[] = params.getTextParts();
+
+        if (textParts.length <= 0) {
+            return emptyCommand(params);
+        }
+
+        String firstArg = textParts[0];
+
+        if (firstArg.equals("status")) {
+            return statusCommand(params);
+        }
+
+        if (firstArg.equals("time")) {
+            return timeCommand(params);
+        }
+
+        if (firstArg.equals("debug")) {
+            return timeCommand(params);
+        }
+
+        return unknownCommand(params);
+
+    }
+
+    public RichMessage unknownCommand(SlackSlashCommandParams params) {
+        RichMessage richMessage = new RichMessage(String.format("Sorry but %s doesn't know the command %s",
+                params.getCommand(), params.getTextParts()[0]));
+        richMessage.setResponseType("ephemeral");
         return richMessage.encodedMessage(); // don't forget to send the encoded message to Slack
     }
 
-    
+    public RichMessage emptyCommand(SlackSlashCommandParams params) {
+        RichMessage richMessage = new RichMessage(
+                String.format("The %s bot knows these commands", params.getCommand()));
+
+        richMessage.setResponseType("ephemeral");
+
+        int numAttachments = 3;
+        Attachment[] attachments = new Attachment[numAttachments];
+        for (int i = 0; i < numAttachments; i++)
+            attachments[i] = new Attachment();
+        attachments[0].setText(String.format("%s status", params.getTeamId()));
+        attachments[1].setText(String.format("%s time", params.getTeamId()));
+        attachments[2].setText(String.format("%s debug", params.getTeamId()));
+
+        return richMessage.encodedMessage(); // don't forget to send the encoded message to Slack
+    }
+
+    public RichMessage statusCommand(SlackSlashCommandParams params) {
+        RichMessage richMessage = new RichMessage(
+                String.format("From: %s Status is ok!", params.getCommand(), params.getTextParts()[0]));
+        richMessage.setResponseType("ephemeral");
+        return richMessage.encodedMessage(); // don't forget to send the encoded message to Slack
+    }
+
+    public RichMessage timeCommand(SlackSlashCommandParams params) {
+        String message = String.format("From: %s... the time on the server is %s", params.getCommand(), timeNow());
+        RichMessage richMessage = new RichMessage(message);
+        richMessage.setResponseType("ephemeral");
+        return richMessage.encodedMessage(); // don't forget to send the encoded message to Slack
+    }
+
+    public RichMessage debugCommand(SlackSlashCommandParams params) {
+        /** build response */
+        RichMessage richMessage = new RichMessage(String.format("Here is what %s knows:", params.getCommand()));
+        richMessage.setResponseType("in_channel"); // other option is "ephemeral"
+
+        // set attachments
+        int numAttachments = 8;
+        Attachment[] attachments = new Attachment[numAttachments];
+        for (int i = 0; i < numAttachments; i++)
+            attachments[i] = new Attachment();
+        attachments[0].setText(String.format("team_id=%s", params.getTeamId()));
+        attachments[1].setText(String.format("team_domain=%s", params.getTeamDomain()));
+        attachments[2].setText(String.format("channel_id=%s", params.getChannelId()));
+        attachments[3].setText(String.format("channel_name=%s", params.getChannelName()));
+        attachments[4].setText(String.format("user_id=%s", params.getUserId()));
+        attachments[5].setText(String.format("user_name=%s", params.getUserName()));
+        attachments[6].setText(String.format("command=%s", params.getCommand()));
+        attachments[7].setText(String.format("text=%s", params.getText()));
+
+        richMessage.setAttachments(attachments);
+
+        return richMessage.encodedMessage(); // don't forget to send the encoded message to Slack
+    }
+
+    private String timeNow() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        return formatter.format(date);
+    }
 
 }
